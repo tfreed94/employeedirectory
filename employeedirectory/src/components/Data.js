1d31ff5bb4f70c0ba2590b49a9817049454c1ffc
Troy Freed <tfreed94@gmail.com>
2021-07-10 15:27:53 -0400

    started setting up data class

--- employeedirectory/src/components/Data.js
+++ employeedirectory/src/components/Data.js
@@ -1,4 +1,22 @@
 import React, { Component } from "react";
 import Table from "../Table";
-import API from '../utils/API'
+import API from '../../utils/API'
 import "../styling/data.css";
+
+class Data extends Component {
+    state = {
+        employee: [],
+        filteredEmployee: [],
+        search: "",
+        sortDirections: this.sortDirection,
+    }
+    get sortDirection() {
+        return {
+            age: '',
+            phone: '',
+            location: '',
+            phone: '',
+            email: '' 
+        }
+    }
+}
