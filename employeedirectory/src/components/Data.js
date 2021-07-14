06748b40b9a1efe8b322f9f325e01b01f99676da
Troy Freed <tfreed94@gmail.com>
2021-07-14 19:12:04 -0400

    trying to fix sorting error

--- employeedirectory/src/components/Data.js
+++ employeedirectory/src/components/Data.js
@@ -28,12 +28,7 @@ class Data extends Component {
     sortEmpInfo = (key) => {
         let sortEmp = this.state.filteredEmp;
         if (this.state.empInfo[key]) {
-            this.setState({
-                filteredEmp: sortEmp.reverse(),
-                empInfo: {
-                    ...this.empInfo,
-                    [key]: this.state.empInfo[key] === "asc" ? "desc" : "asc",
-                },
+            this.setState({filteredEmp: sortEmp.reverse(),
             });
         }
         this.setState({
