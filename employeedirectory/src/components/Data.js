d6d023e0db639cd320de93d32a3ecaef9ab73528
Troy Freed <tfreed94@gmail.com>
2021-07-14 11:46:31 -0400

    fixed import for Table

--- employeedirectory/src/components/Data.js
+++ employeedirectory/src/components/Data.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Table from "../Table";
+import Table from "../components/Table";
 import API from '../../utils/API'
 import "../styling/data.css";
 
