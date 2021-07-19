fdae989b35a0cbee1518bfaffb7e8dec2581496c
Troy Freed <tfreed94@gmail.com>
2021-07-19 17:58:09 -0400

    removed content referring to sorting/filtering because I cant get my sort/filter to work properly

--- employeedirectory/src/components/Header.js
+++ employeedirectory/src/components/Header.js
@@ -4,8 +4,6 @@ const header = () => {
     return (
         <div className="text-center">
             <h1>React Employee-Directory</h1>
-            <h4>The Directory!</h4>
-            <p>With the icons you can search by name or location!</p>
         </div>
     )
 }
